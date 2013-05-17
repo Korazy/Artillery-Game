13fa6235b5551565be5a66e06b5c4787efd49b04
ringadingding <ringadingding@fakemail.info>
2013-05-17 20:41:09 +0400

    Added random terrain generation.

--- ArtilleryGame/src/artillerygame/ArtilleryGame.java
+++ ArtilleryGame/src/artillerygame/ArtilleryGame.java
@@ -1,9 +1,17 @@
 package artillerygame;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
 public class ArtilleryGame {
-
     public static void main(String[] args) {
-        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
     }
-
 }
